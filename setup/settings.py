97cbd2f2fa6e24925ce2d41a65319669f501853f
Marcus Vieira <marcusvmvieira@gmail.com>
2025-02-04 18:24:14 -0300

    Inclusão das pastas para manipulação de arquivos estátics

--- setup/settings.py
+++ setup/settings.py
@@ -122,6 +122,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Origem dos arquivos estáticos
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'setup/static')]
+
+# Caminho absoluto do diretório par amanipulação dos arquivos estáticos
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
